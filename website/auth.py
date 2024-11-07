40e3dd2651e831a41d54c0e5a21d5450a3706ad5
Zachary Potthoff <zach.potthoff@icloud.com>
2024-11-07 14:39:37 -0500

    able to login, doesn't take user to account yet

--- website/auth.py
+++ website/auth.py
@@ -11,8 +11,21 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST']) # defines url for where to login
 def login():
-    data = request.form
-    print(data)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # check if user exists
+        user = User.query.filter_by(email=email).first()
+        if user: # if we found a user
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
     return render_template("login.html")
 
 @auth.route('/logout') # defines url for where to logout
@@ -27,8 +40,13 @@ def sign_up():
         last_name = request.form.get('lastName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
+
+        user = User.query.filter_by(email=email).first()
         
-        if len(email) < 4:
+        # check for problems with signing up, if none, add user to DB
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters', category='error')
         elif len(first_name) < 2:
             flash('First Name must be greater than 1 character', category='error')
