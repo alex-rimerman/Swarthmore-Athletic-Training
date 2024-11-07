c0472c7c3d436c45f88e455800ee0ed2407c5f6f
Zach Potthoff <zach.potthoff@icloud.com>
2024-11-06 21:30:16 -0500

    sign in feature added

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 # define a blueprint for views
 # means we have a collection of routes/urls that are related to each other
@@ -20,25 +23,28 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
-        lastName = request.form.get('lastName')
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
         if len(email) < 4:
             flash('Email must be greater than 3 characters', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First Name must be greater than 1 character', category='error')
-        elif len(lastName) < 2:
+        elif len(last_name) < 2:
             flash('Last Name must be greater than 1 character', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match', category='error')
         elif len(password1) < 7:
             flash('Password must be greater than 6 character', category='error')
         else:
+            # add user to DB  
+            new_user = User(email=email, first_name=first_name, last_name=last_name,
+                            password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
-            # add user to DB    
+            return redirect(url_for('views.home'))  
             
-
-   
     return render_template("sign_up.html")
