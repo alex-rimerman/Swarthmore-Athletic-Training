a800c51ec55a621c19c769a089bba018ea2c8861
alex-rimerman <alex.rimerman@gmail.com>
2024-11-06 21:09:04 -0500

    Made the DB

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "users.db"
 
 def create_app():
     app = Flask(__name__) # initialize Flask app, __name__ is name of file that was ran
     app.config['SECRET_KEY'] = 'ksfjdkh shkd' # secret key for session management, would want this to be well generated/hidden in production
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views # import views blueprint from views.py
     from .auth import auth # import auth blueprint from auth.py
@@ -11,4 +18,14 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/') 
 
+    from .models import User, Note
+
+    create_database(app)
+
     return app
+
+def create_database(app):
+    with app.app_context():
+        if not path.exists('website/' + DB_NAME):
+            db.create_all()
+            print('Created Database!')
